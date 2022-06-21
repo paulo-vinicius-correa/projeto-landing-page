4f125dd6c068f7bf9e62f10f50cf8f2c3e71b085
Paulo Vinicius Correa <107892992+paulo-vinicius-correa@users.noreply.github.com>
2022-06-20 21:25:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new swiper(".mySwiper",{
+    cssMode: true,
+    loop: true,
+    navgation: {
+nextEl: =".swiper-button-next",
+prevEl: =".swiper-button-prev",   
+},
+pagination: {
+    el: ".swiper-pagination",
+},
+mousewheel: true,
+keyboard: true, 
+});
